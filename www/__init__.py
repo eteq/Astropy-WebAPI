e5518808c47d9f4b4a5604c9609a270a5a19d7ce
Erik Tollerud <erik.tollerud@gmail.com>
2014-12-10 00:26:01 -0600

    Code cleanups

--- www/__init__.py
+++ www/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 #-----------------------------------------------------------------------------
-#  Copyright (C) 2014 mashta
+#  Copyright (C) 2014 darn + eteq
 #
 #  Distributed under the terms of the BSD License.  The full license is in
 #  the file LICENSE.txt, distributed as part of this software.
@@ -11,60 +11,31 @@ from __future__ import division, print_function
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Standard library
-import os, sys
+import os
+import sys
 import time
 import random
 
 # Third-party
-from astropy.utils import isiterable
-import astropy.coordinates as coord
 import astropy.units as u
-from astropy.coordinates.sky_coordinate import _get_frame, _get_frame_class
+from astropy.utils import isiterable
+import astropy.coordinates import SkyCoord, frame_transform_graph
 from flask import Flask, request, render_template, session, redirect, abort, flash, jsonify
 
 with open(".env") as f:
     for line in f:
-        key,val = line.strip().split("=")
+        key, val = line.strip().split("=")
         os.environ[key] = val
 
 app = Flask(__name__)   # create our flask app
 app.secret_key = os.environ.get('SECRET_KEY')
 
-# @app.route('/')
-# @app.route('/<tag>')
-# def index(tag=None):
-
-#     if 'instagram_access_token' in session and 'instagram_user' in session:
-#         # if no tag specified, get a recent popular image, grab the tag from that
-#         if tag is None:
-#             while True:
-#                 media = api.media_popular(1, None)
-
-#                 if hasattr(media[0], 'tags') and len(media[0].tags) > 0:
-#                     break
-#                 time.sleep(0.1)
-
-#             tag = random.choice(media[0].tags).name
-
-#         media, next = api.tag_recent_media(2, None, tag)
-#         print(next)
-
-#         templateData = {
-#             'size' : request.args.get('size','thumb'),
-#             'media' : media,
-#             'header' : tag.lower()
-#         }
-
-#         return render_template('display.html', **templateData)
-
-#     else:
-#         return redirect('/connect')
 
 @app.route('/api/convert', methods=['GET', 'POST'])
 def convert():
     if request.method == 'GET':
         args = dict(request.args)
-        for k,v in args.items():
+        for k, v in args.items():
             if len(v) == 1:
                 args[k] = v[0]
 
@@ -80,7 +51,7 @@ def convert():
     # first try pulling out separate coordinates
     c1 = args.pop('coord1', None)
     c2 = args.pop('coord2', None)
-    cargs = (c1,c2)
+    cargs = (c1, c2)
 
     # if either are None, check for a coordstr
     if c1 is None or c2 is None:
@@ -101,7 +72,7 @@ def convert():
     # extra arguments to pass to the coordinate frames
     fromargs = dict()
     toargs = dict()
-    for k,v in args.items():
+    for k, v in args.items():
         if k.startswith('from_'):
             fromargs[k.split('from_')[1]] = v
 
@@ -111,9 +82,9 @@ def convert():
     fromargs['frame'] = from_system.lower()
 
     # get the 'to' frame class
-    ToFrame = _get_frame_class(to_system.lower())
+    ToFrame = frame_transform_graph.lookup_name(to_system.lower())
 
-    c = coord.SkyCoord(*cargs, unit=(c1u,c2u), **fromargs)\
+    c = coord.SkyCoord(*cargs, unit=(c1u, c2u), **fromargs)\
              .transform_to(frame=ToFrame(**toargs))
 
     c1_out = c.data.lon.to(c1u).value
