8c0a92595566fd6bb6875d88d985b781a044e41c
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-01-09 16:55:07 -0600

    cleaned up parsing

--- twitterbot/parse.py
+++ twitterbot/parse.py
@@ -25,14 +25,36 @@ __all__ = ['parse_unit', 'convert_unit_tweet']
 p = inflect.engine()
 
 def parse_unit(tweet_text):
-    """ Search all words for valid units, check preceding number """
+    """ Given a string block of text, find any words that can be
+        interpreted as units. If the unit is preceded by a number,
+        turn the pair into an Astropy Quantity. If the unit string
+        is by itself, turn it into an Astropy Unit.
+
+        Parameters
+        ----------
+        tweet_text : str
+            A block of whitespace delimited text, e.g., a tweet.
+
+        Returns
+        -------
+        units : list of :class:`astropy.units.Unit`
+            A list of any bare units parsed from the text.
+        quantites : list of :class:`astropy.units.Quantity`
+            A list of any number+unit pairs, turned into Quantity objects.
+    """
+
     words = word_tokenize(tweet_text)
 
+    units = []
     quantities = []
     for i,word in enumerate(words):
+        sing_word = p.singular_noun(word)
+        if sing_word is False or "/" in word:
+            sing_word = word
+
         # identify type of word
         try:
-            unit = u.Unit(p.singular_noun(word))
+            unit = u.Unit(sing_word)
             if unit.physical_type == 'dimensionless':
                 raise ValueError()
 
@@ -48,22 +70,27 @@ def parse_unit(tweet_text):
                 quantities.append(value * unit)
             except ValueError:
                 # word preceding not value
-                quantities.append(unit)
+                units.append(unit)
 
-        except:
+        except ValueError:
+            # print("error", word, sing_word)
             # print("{} not a valid unit".format(word))
             continue
 
-    return quantities
+    return units, quantities
 
 def convert_unit_tweet(tweet_text):
 
     try:
-        quantity,unit = parse_unit(tweet_text)
+        units,quantities = parse_unit(tweet_text)
     except ValueError:
         # TODO:
         return
 
-    q = quantity.to(unit)
+    if len(quantities) > 1 or len(units) > 1:
+        # TODO: warning?
+        pass
+
+    q = quantities[0].to(units[0])
 
     return "{} {}".format(q.value, q.unit)
--- twitterbot/tests/test_parse.py
+++ twitterbot/tests/test_parse.py
@@ -4,29 +4,35 @@ from __future__ import division, print_function
 
 __author__ = "adrn <adrn@astro.columbia.edu>"
 
-# Standard library
-import os
-import sys
-
 # Third-party
-import numpy as np
-import pytest
+import astropy.units as u
 
 from ..parse import parse_unit, convert_unit_tweet
 
-def test_parse_units():
+convert_tweets = ["Convert 15 lightyears to parsecs",
+                  "Convert 15 km/s to pc/Myr"]
+expected = [(u.parsec, 15.*u.lightyear),
+            (u.pc/u.Myr, 15*u.km/u.s)]
+def test_parse_unit():
+
+    # ---
     tweet = "This star is 15 lightyears away! http://t.co/kmsdfkn"
-    print(parse_unit(tweet))
+    us,qs = parse_unit(tweet)
+    assert len(us) == 0
+    assert len(qs) == 1
+    assert qs[0] == u.Quantity(15.*u.lightyear)
 
-    tweet = "Convert 15 lightyears to parsecs"
-    print(parse_unit(tweet))
+    # ---
+    for i,tweet in enumerate(convert_tweets):
+        us,qs = parse_unit(tweet)
+        assert len(us) == 1
+        assert len(qs) == 1
+        assert us[0] == expected[i][0]
+        assert qs[0] == expected[i][1]
 
-def test_convert_tweet():
-    tweet = "Convert 15 lightyears to parsecs"
-    print(convert_unit_tweet(tweet))
-
-    tweet = "Convert 15 minutes to picoseconds"
-    print(convert_unit_tweet(tweet))
-
-
-
+def test_convert_unit_tweet():
+    for i,tweet in enumerate(convert_tweets):
+        resp = convert_unit_tweet(tweet)
+        q = expected[i][1].to(expected[i][0])
+        expected_resp = "{} {}".format(q.value, q.unit)
+        assert resp == expected_resp
